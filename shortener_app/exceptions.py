daaeec4d4370b899be0c9a4646b752f1d17dcd67
Sergio Mondragon <smondragons@valtx.pe>
2024-02-07 17:15:37 -0500

    Add exception handling and URL validation

---
+++ shortener_app/exceptions.py
@@ -0,0 +1,4 @@
+from fastapi import HTTPException
+
+def raise_bad_request(message):
+    raise HTTPException(status_code=400, detail=message)
--- shortener_app/main.py
+++ shortener_app/main.py
@@ -1,10 +1,22 @@
+import validators
 from fastapi import FastAPI,Depends
 from typing import Annotated
-from .config import get_settings
+from .config import get_settings,Settings
+from .exceptions import raise_bad_request
+from . import schemas
 
-settings = get_settings()
 app = FastAPI()
 
 @app.get("/info")
-async def info(settings:Annotated[settings, Depends(get_settings)]):
+async def info(settings:Annotated[Settings, Depends(get_settings)]):
     return {"app_name": settings.app_name}
+
+@app.get("/")
+def home(settings:Annotated[Settings, Depends(get_settings)]):
+    return {"message": f"Hello World from app: {settings.app_name}"}
+
+@app.post("/url")
+def create_url(url:schemas.URLBase):
+    if not validators.url(url.target_url):
+        raise_bad_request("Invalid URL")
+    return {"target_url": url.target_url}
